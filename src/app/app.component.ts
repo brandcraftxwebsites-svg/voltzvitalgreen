203c82ef163c1054b02b0d6c29cffc757bcdd4a4
Ashfaq Ahamed <ashfaqahamed.ASJTech@almatar.com>
2026-07-11 13:41:40 +0530

    maintenance for voltvitalgreen

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,10 +8,7 @@ import { FooterComponent } from './shared/components/footer/footer.component';
   selector: 'app-root',
   standalone: true,
   imports: [RouterOutlet, NavbarComponent, FooterComponent],
-  template: `
-    <app-navbar></app-navbar>
-    <router-outlet></router-outlet>
-    <app-footer></app-footer>
-  `
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
 })
 export class AppComponent {}
